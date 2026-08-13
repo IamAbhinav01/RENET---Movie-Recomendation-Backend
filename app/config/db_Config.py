8085c8c45aef343941290cd84f78a8cf336ecd3f
Abhinav <AbhinavSunil@hotmail.com>
2026-08-14 01:10:44 +0530

    Feat : Added the schema for postgres

--- app/config/db_Config.py
+++ app/config/db_Config.py
@@ -1,6 +1,7 @@
 from sqlalchemy import create_engine, text
 from server_Config import server_config
 from logger_Config import setup_logger
+from schemas.postgres_schema import Base
 
 settings = server_config()
 logger = setup_logger(name="RENET-DBCONFIG")
@@ -11,5 +12,7 @@ try:
     with engine.connect() as conn:
         conn.execute(text("SELECT 1"))
     logger.info("successfully connected to POSTGRESQL")
+    Base.metadata.create_all(bind = engine)
+    logger.info("Database tables ready.")
 except Exception as e:
     logger.error(f"Error while connecting to the DB, error -> {e}")
